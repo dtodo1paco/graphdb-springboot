8d775e9e1c71c90f7fdc5d1edccde5f66b644147
Paco Alías <dtodo1paco@gmail.com>
2021-11-15 11:27:33 +0100

    Addenda for s4e1 using parameters to change the query

--- src/main/java/org/dtodo1paco/samples/graphdb/controllers/ExercisesController.java
+++ src/main/java/org/dtodo1paco/samples/graphdb/controllers/ExercisesController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController()
 @RequestMapping("exercises")
@@ -41,7 +43,7 @@ public class ExercisesController {
     String query = "MATCH (p1:Person)-[:HAS_CONTACT]-(p2) " +
       "OPTIONAL MATCH (p2)-[:DIRECTED]->(m)"+
        "RETURN p1.name as person, p2.name as director, m.title as movieTitle";
-    return db.findItems(query, limit);
+    return db.findItems(query, null, limit);
   }
 
   @GetMapping("/section_04/exercise_01")
@@ -52,6 +54,25 @@ public class ExercisesController {
       + "MATCH (contact)-[role:ACTED_IN]->(movie:Movie) "
       + "WHERE contact.born >= 1960 AND role.earnings > 10000000 "
       + "RETURN contact.name, contact.born, movie.title, role.earnings ";
-    return db.findItems(query, limit);
+    return db.findItems(query, null, limit);
   }
+
+  @GetMapping("/section_04/exercise_01_addenda")
+  public Collection<ItemProperties> findActorContactsBornAfterYearAndEarntOverAmountIn1Movie(
+    @RequestParam(value = "name", defaultValue = "Tom Hanks") String name,
+    @RequestParam(value = "year", defaultValue = "1960") Integer year,
+    @RequestParam(value = "earnings", defaultValue = "10000000") Long earnings,
+    @RequestParam(value = "limit", defaultValue = "3") Integer limit) {
+    String query = "MATCH (actor:Person{name: $name}) "
+                     + "MATCH (actor)-[:HAS_CONTACT]->(contact:Person) "
+                     + "MATCH (contact)-[role:ACTED_IN]->(movie:Movie) "
+                     + "WHERE contact.born >= $year AND role.earnings > $earnings "
+                     + "RETURN contact.name, contact.born, movie.title, role.earnings ";
+    Map<String, Object> params = new HashMap<>();
+    params.put("name", name);
+    params.put("year", year);
+    params.put("earnings", earnings);
+    return db.findItems(query, params, limit);
+  }
+
 }
